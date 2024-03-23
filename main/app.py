4d3bc79c2760805f807164aab974740ee77967e6
Praveen Peiris <129660222+peirispraveen@users.noreply.github.com>
2024-03-23 21:54:45 +0530

    token iterate

--- main/app.py
+++ main/app.py
@@ -4,3 +4,12 @@ from collections import defaultdict
 tokens = ["I", "try", "to", "learn", "something", "new", "every", "day"]
 
 graph = defaultdict(list)
+
+graph["word"].append("hello")
+
+print(graph["word"])
+
+for i, token in enumerate(tokens):
+    print(i, token)
+
+print(random.choice(tokens))
